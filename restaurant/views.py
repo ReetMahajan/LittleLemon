d71049838367694217d50e21a23ee729143fece7
unknown <mahajanreet7@gmail.com>
2025-06-28 16:36:25 +0530

    Add booking API and menu API using DRF

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Little Lemon API</h1>")
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {})
+
+# Handles GET (list) and POST (create)
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# Handles GET (retrieve), PUT (update), DELETE (delete)
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
